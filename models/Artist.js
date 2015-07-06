444b5a264aa781135e73e122a49231b81079dd6d
John Resig <jeresig@gmail.com>
2015-07-05 21:55:17 -0700

    Start to port over some of the properties from the ukiyoe-models models.

--- models/Artist.js
+++ models/Artist.js
@@ -1,7 +1,38 @@
 var mongoose = require("mongoose");
 
 var ArtistSchema = new mongoose.Schema({
-    
+    // The date that this item was created
+    created: {type: Date, "default": Date.now},
+
+    // The date that this item was updated
+    modified: Date,
+
+    // The name of the artist
+    names: {type: [Name], es_indexed: true},
+    aliases: {type: [Name], es_indexed: true},
+
+    bios: [{type: String, ref: "Bio"}],
+
+    // An image that is representative of the artist's work
+    repImage: {type: String, ref: "Image"},
+
+    // An image depicting the artist
+    artistImage: {type: String, ref: "Image"},
+
+    // Eras in which the artist was active
+    eras: [{type: String, ref: "Era"}],
+
+    // The location of the matching VIAF record for this artist
+    // or from the Getty Union List of Artist Names
+    canonical: [String],
+
+    // Locations in which the artist was active
+    locations: {type: [String], es_indexed: true},
+
+    actives: [YearRange],
+    lives: [YearRange],
+
+    gender: {type: String, es_indexed: true}
 });
 
 mongoose.model("Artist", ArtistSchema);
