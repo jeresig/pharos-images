f3cf31e07a9beeb60d7879275be4d4191944d4ca
John Resig <jeresig@gmail.com>
2015-07-10 10:02:02 -0700

    Start to stub out Zeri convertor.

---
+++ converters/fzeri.js
@@ -0,0 +1,19 @@
+/*
+ * ID (SERCD)
+ * Title/Subject (SGTI)
+ * Date (DTSI - from, DTSF - to, DTSV/DTSL - approximation)
+ * Category ?
+ * Medium/Material (MTC)
+ * Object Type (OGTT)
+ * Dimensions (MISA - height, MISL - width, MISU - unit)
+ * Collection (LCDN)
+ * Collection Location (PVCS + PVCC)
+ * For each RIPETIZIONE:
+ * Artist (AUTN + AUTP for pseudonym)
+ * Artist Dates/School?
+ */
+
+/*
+ * ID (FOTO/@sercdf)
+ * Image Path (FOTO)
+ */
