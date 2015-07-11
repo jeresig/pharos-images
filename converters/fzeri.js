e2a78023c78c3af0924ba0e0ff617386fd4dc2b5
John Resig <jeresig@gmail.com>
2015-07-11 15:47:36 -0700

    Start to bring in some XML processing code.

--- converters/fzeri.js
+++ converters/fzeri.js
@@ -17,3 +17,18 @@
  * ID (FOTO/@sercdf)
  * Image Path (FOTO)
  */
+var fs = require("fs");
+var libxmljs = require("libxmljs");
+
+var file = process.argv[2];
+
+var xmlDoc = libxmljs.parseXml(fs.readFileSync(file, "utf8"));
+
+xmlDoc.find("//SCHEDA").forEach(function(node) {
+    //var id = node.attr("sercdoa").value();
+
+    var id = node.get(".//SERCD").text();
+    var imageID = /\/([\w\d]+).jpg/.exec(path)[1];
+
+    map[id].push(imageID);
+});
