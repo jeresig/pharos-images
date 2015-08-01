b754aca3bc2a4599a06dddd0cf3193ecd579c67f
John Resig <jeresig@gmail.com>
2015-07-31 17:07:48 -0700

    Dump the MARC records as cleaner JSON objects for inspection.

--- converters/marc.js
+++ converters/marc.js
@@ -6,7 +6,7 @@ var stream = fs.createReadStream(process.argv[2]);
 var reader = new marc.getReader(stream, "iso2709");
 
 reader.on("data", function(record) {
-    console.log(record);
+    console.log(JSON.stringify(record.toMiJ(), null, "    "));
 });
 
 reader.on("end", function() {
