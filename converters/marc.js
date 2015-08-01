18236135c115c6b29d275d8ba4ad1498ce2b95d7
John Resig <jeresig@gmail.com>
2015-08-01 09:52:46 -0700

    Figure out field mapping for MARC records.

--- converters/marc.js
+++ converters/marc.js
@@ -2,6 +2,18 @@ var fs = require("fs");
 
 var marc = require("marcjs");
 
+var propMap = {
+    id: ["001"],
+    title: ["245", "a"],
+    dateCreated: ["260", "c"],
+    categories: ["650", "a", "y", "z"],
+    material: ["300", "a"],
+    artists: ["100", "a", "d"],
+    dimensions: ["300", "c"],
+    collections: ["710", "a"],
+    images: ["856", "u"]
+};
+
 var stream = fs.createReadStream(process.argv[2]);
 var reader = new marc.getReader(stream, "iso2709");
 
