9cc994521fba75dbd7a85af604cdfdeea01c7900
John Resig <jeresig@gmail.com>
2015-09-18 21:51:30 +0100

    Bring in auto-generated locale files.

---
+++ locales/en.js
@@ -0,0 +1,16 @@
+{
+	"Japanese Woodblock Print Search": "Japanese Woodblock Print Search",
+	"Japanese Woodblock print search engine. Searches thousands of Ukiyo-e, Meiji, Shin Hanga, and Sosaku Hanga prints.": "Japanese Woodblock print search engine. Searches thousands of Ukiyo-e, Meiji, Shin Hanga, and Sosaku Hanga prints.",
+	"en-US": "en-US",
+	"Search": "Search",
+	"Sources": "Sources",
+	"Blog": "Blog",
+	"About": "About",
+	"Ukiyo-e Search provides an incredible resource: The ability to both search for Japanese woodblock prints by simply taking a picture of an existing print AND the ability to see similar prints across multiple collections of prints. Below is an example print, click to see it in action.": "Ukiyo-e Search provides an incredible resource: The ability to both search for Japanese woodblock prints by simply taking a picture of an existing print AND the ability to see similar prints across multiple collections of prints. Below is an example print, click to see it in action.",
+	"Search %s prints:": "Search %s prints:",
+	"Search by Image:": "Search by Image:",
+	"Upload a picture of a print to find similar prints across multiple collections.": "Upload a picture of a print to find similar prints across multiple collections.",
+	"Upload an Image": "Upload an Image",
+	"Paste image URL": "Paste image URL",
+	"Search by Image": "Search by Image"
+}
