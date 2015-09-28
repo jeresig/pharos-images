9c6a2abb1e240945a435f4158a00a2deaba922db
John Resig <jeresig@gmail.com>
2015-09-28 16:14:31 -0400

    Add a utility for mapping and importing the artworks into Elasticsearch.

---
+++ utils/init-es.js
@@ -0,0 +1,20 @@
+var core = require("../models");
+
+var Artwork = core.db.model("Artwork");
+
+core.init(function() {
+    Artwork.createMapping(function(err, mapping) {
+        var stream = Artwork.synchronize();
+        var count = 0;
+        stream.on('data', function(err, doc){
+            count++;
+            //console.log('indexed ' + count);
+        });
+        stream.on('close', function(){
+            process.exit(0);
+        });
+        stream.on('error', function(err){
+            console.log(err);
+        });
+    });
+});
