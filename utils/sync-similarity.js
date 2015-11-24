8e1a0bfcf5b8dc3636250f6cff2a3b4c3404840d
John Resig <jeresig@gmail.com>
2015-11-23 21:01:00 -0500

    Fix up some bugs in the image similarity syncing script.

--- utils/sync-similarity.js
+++ utils/sync-similarity.js
@@ -15,10 +15,15 @@ core.init(() => {
     console.log("Finding artwork records...");
 
     Artwork.find().stream()
-        .on("data", (artwork) => {
+        .on("data", function(artwork) {
             this.pause();
+
+            console.log("Syncing", artwork._id);
+
             async.mapLimit(artwork.images, 1, (image, callback) => {
                 pastec.similar(image.imageName, (err, matches) => {
+                    matches = matches.filter((match) =>
+                        match.id.toString() !== image.imageName);
                     image.similarImages = matches;
                     callback(err, matches);
                 });
@@ -42,8 +47,10 @@ core.init(() => {
                             return;
                         }
 
-                        artwork.similarArtworks =
-                            artworks.map((artwork) => artwork._id);
+                        artwork.similarArtworks = artworks
+                            .map((similar) => similar._id)
+                            .filter((id) => id !== artwork._id);
+
                         artwork.save(() => this.resume());
                     });
                 }
