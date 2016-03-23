222e484753731d5a6b082e26b726fbf6afd757ba
John Resig <jeresig@gmail.com>
2016-03-22 23:38:50 -0400

    Add some coverage tests for the artwork pages.

--- tests/logic/artworks.js
+++ tests/logic/artworks.js
@@ -13,3 +13,66 @@ tap.test("Search", (t) => {
         t.end();
     });
 });
+
+tap.test("By Type", (t) => {
+    const url = "http://localhost:3000/type/painting";
+    request.get(url, (err, res) => {
+        t.error(err, "Error should be empty.");
+        t.equal(res.statusCode, 200);
+        t.end();
+    });
+});
+
+tap.test("By Type Missing", (t) => {
+    const url = "http://localhost:3000/type/foo";
+    request.get(url, (err, res) => {
+        t.error(err, "Error should be empty.");
+        t.equal(res.statusCode, 404);
+        t.end();
+    });
+});
+
+tap.test("By Source", (t) => {
+    const url = "http://localhost:3000/source/test";
+    request.get(url, (err, res) => {
+        t.error(err, "Error should be empty.");
+        t.equal(res.statusCode, 200);
+        t.end();
+    });
+});
+
+tap.test("By Source Missing", (t) => {
+    const url = "http://localhost:3000/source/foo";
+    request.get(url, (err, res) => {
+        t.error(err, "Error should be empty.");
+        t.equal(res.statusCode, 404);
+        t.end();
+    });
+});
+
+tap.test("Artwork", (t) => {
+    const url = "http://localhost:3000/artworks/test/1234";
+    request.get(url, (err, res) => {
+        t.error(err, "Error should be empty.");
+        t.equal(res.statusCode, 200);
+        t.end();
+    });
+});
+
+tap.test("Artwork Compare", (t) => {
+    const url = "http://localhost:3000/artworks/test/1234?compare=1";
+    request.get(url, (err, res) => {
+        t.error(err, "Error should be empty.");
+        t.equal(res.statusCode, 200);
+        t.end();
+    });
+});
+
+tap.test("Artwork Missing", (t) => {
+    const url = "http://localhost:3000/artworks/test/foo";
+    request.get(url, (err, res) => {
+        t.error(err, "Error should be empty.");
+        t.equal(res.statusCode, 404);
+        t.end();
+    });
+});
