14ae80c0718e197599bee4007109a3b6657d21c9
John Resig <jeresig@gmail.com>
2016-04-19 15:43:42 -0400

    Add in support for extracting the locale from subdomains.

--- server/i18n.js
+++ server/i18n.js
@@ -2,7 +2,9 @@
 
 const i18n = require("i18n-abide");
 
+const config = require("../lib/config");
 const locales = require("../config/locales.json");
+const defaultLocale = Object.keys(locales)[0];
 
 module.exports = (app) => {
     app.use((req, res, next) => {
@@ -14,7 +16,7 @@ module.exports = (app) => {
 
     app.use(i18n.abide({
         supported_languages: Object.keys(locales),
-        default_lang: "en",
+        default_lang: defaultLocale,
         translation_directory: "translations",
         translation_type: "po",
     }));
@@ -23,9 +25,20 @@ module.exports = (app) => {
         // Restore the old local properties and methods
         Object.assign(res.locals, res.tmpLocals);
 
-        // Set the locale based upon the ?lang= query string
-        // TODO: Check sub-domain for language
-        res.locals.setLocale(req.query.lang || "en");
+        let locale = config.USE_I18N_SUBDOMAIN === "1" ?
+            // Set the locale based upon the subdomain
+            /* istanbul ignore else */
+            (/\/\/(\w+)/.exec(req.url) || [])[0] :
+
+            // Set the locale based upon the ?lang= query string
+            req.query.lang;
+
+        // Fall back to the default locale if one isn't given, or it's invalid
+        if (!locales[locale]) {
+            locale = defaultLocale;
+        }
+
+        res.locals.setLocale(locale);
 
         next();
     });
