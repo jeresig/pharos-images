c754c9cc7f9b916c9a64eb56c327a3f98d8968d8
John Resig <jeresig@gmail.com>
2016-04-29 22:11:39 -0400

    Set Upload to not inherit from Artwork.

--- schemas/Upload.js
+++ schemas/Upload.js
@@ -2,18 +2,38 @@
 
 const async = require("async");
 
+const db = require("../lib/db");
 const models = require("../lib/models");
 const urls = require("../lib/urls");
 
 const Artwork = require("./Artwork");
 
-const uploadName = "uploads";
+const Upload = new db.schema({
+    // UUID of the image (Format: uploads/imageName)
+    _id: {
+        type: String,
+        es_indexed: true,
+    },
 
-const Upload = Artwork.extend({
+    // The date that this item was created
+    created: {
+        type: Date,
+        default: Date.now,
+    },
+
+    // The date that this item was updated
+    modified: Date,
+
     // Source is always set to "uploads"
     source: {
         type: String,
-        default: uploadName,
+        default: "uploads",
+        required: true,
+    },
+
+    // A hash to use to render an image representing the artwork
+    defaultImageHash: {
+        type: String,
         required: true,
     },
 
@@ -22,57 +42,83 @@ const Upload = Artwork.extend({
         type: [{type: String, ref: "UploadImage"}],
         required: true,
     },
-}, {
-    collection: uploadName,
+
+    // Computed by looking at the results of images.similarImages
+    similarArtworks: [{
+        _id: String,
+
+        artwork: {
+            type: String,
+            ref: "Artwork",
+            required: true,
+        },
+
+        images: {
+            type: [String],
+            required: true,
+        },
+
+        source: {
+            type: String,
+            es_indexed: true,
+            required: true,
+        },
+
+        score: {
+            type: Number,
+            es_indexed: true,
+            required: true,
+            min: 1,
+        },
+    }],
 });
 
-Upload.methods.getTitle = function(req) {
-    return req.gettext("Uploaded Image");
-};
+Upload.methods = Object.assign({}, Artwork.methods, {
+    getTitle(req) {
+        return req.gettext("Uploaded Image");
+    },
 
-Upload.methods.getURL = function(locale) {
-    return urls.gen(locale, `/${this._id}`);
-};
+    getURL(locale) {
+        return urls.gen(locale, `/${this._id}`);
+    },
 
-Upload.methods.getImages = function(callback) {
-    async.mapLimit(this.images, 4, (id, callback) => {
-        if (typeof id !== "string") {
-            return process.nextTick(() => callback(null, id));
-        }
-        models("UploadImage").findById(id, callback);
-    }, callback);
-};
+    getImages(callback) {
+        async.mapLimit(this.images, 4, (id, callback) => {
+            if (typeof id !== "string") {
+                return process.nextTick(() => callback(null, id));
+            }
+            models("UploadImage").findById(id, callback);
+        }, callback);
+    },
+});
 
-Upload.statics.fromImage = function(image, callback) {
-    const Upload = models("Upload");
+Upload.statics = Object.assign({}, Artwork.statics, {
+    fromImage(image, callback) {
+        const Upload = models("Upload");
 
-    const _id = image._id.replace(/\.jpg$/, "");
-    const id = _id.replace(`${uploadName}/`, "");
+        const _id = image._id.replace(/\.jpg$/, "");
 
-    // Check to see if image already exists and redirect
-    // if it does.
-    Upload.findById(_id, (err, existing) => {
-        /* istanbul ignore if */
-        if (err) {
-            return callback(err);
-        }
+        // Check to see if image already exists and redirect
+        // if it does.
+        Upload.findById(_id, (err, existing) => {
+            /* istanbul ignore if */
+            if (err) {
+                return callback(err);
+            }
 
-        if (existing) {
-            return callback(null, existing);
-        }
+            if (existing) {
+                return callback(null, existing);
+            }
 
-        const upload = new Upload({
-            _id,
-            id,
-            source: "uploads",
-            images: [image._id],
-            defaultImageHash: image.hash,
-            url: image.getOriginalURL(),
-            lang: "en",
+            const upload = new Upload({
+                _id,
+                images: [image._id],
+                defaultImageHash: image.hash,
+            });
+
+            callback(null, upload);
         });
-
-        callback(null, upload);
-    });
-};
+    },
+});
 
 module.exports = Upload;
