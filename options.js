5b3a95bfc6a4a8e026a8e8ec1b829d6c5a275d7d
John Resig <jeresig@gmail.com>
2016-07-11 20:09:50 -0700

    Remove model definitions, have them be instantiated dynamically.

--- options.js
+++ options.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const types = require("./custom/types");
+
 module.exports = {
     getShortTitle: () => "PHAROS",
 
@@ -19,5 +21,81 @@ module.exports = {
     sorts: {
         "dates.asc": (i18n) => i18n.gettext("Date, earliest first"),
         "dates.desc": (i18n) => i18n.gettext("Date, latest first"),
+    },
+
+    model: {
+        // The title of the artwork.
+        title: {
+            type: "SimpleString",
+            title: (i18n) => i18n.gettext("Title"),
+            recommended: true,
+        },
+
+        // A list of artist names extracted from the page.
+        artists: {
+            type: "Name",
+            searchName: "artist",
+            title: (i18n) => i18n.gettext("Artist"),
+            placeholder: (i18n) => i18n.gettext("Sample: Andrea del Sarto"),
+        },
+
+        // Date ranges when the artwork was created or modified.
+        dates: {
+            type: "YearRange",
+            searchName: "date",
+            title: (i18n) => i18n.gettext("Date"),
+            placeholder: () => ({
+                end: 1900,
+                start: 1000,
+            }),
+        },
+
+        // The English form of the object type (e.g. painting, print)
+        objectType: {
+            type: "FixedString",
+            searchName: "type",
+            title: (i18n) => i18n.gettext("Type"),
+            placeholder: (i18n) => i18n.gettext("Any Type"),
+            allowUnknown: false,
+            values: types,
+            recommended: true,
+            url: (value) => `/type/${value}`,
+        },
+
+        // The medium of the artwork (e.g. "watercolor")
+        medium: {
+            type: "SimpleString",
+            title: (i18n) => i18n.gettext("Medium"),
+            searchField: "filter",
+        },
+
+        // The size of the artwork (e.g. 100mm x 200mm)
+        dimensions: {
+            type: "Dimension",
+            title: (i18n) => i18n.gettext("Dimensions"),
+            heightTitle: (i18n) => i18n.gettext("Height"),
+            widthTitle: (i18n) => i18n.gettext("Width"),
+            placeholder: () => ({
+                max: 200,
+                min: 10,
+            }),
+        },
+
+        // Locations where the artwork is stored
+        locations: {
+            type: "Location",
+            searchName: "location",
+            title: (i18n) => i18n.gettext("Location"),
+            placeholder: (i18n) => i18n.gettext("Sample: Louvre"),
+        },
+
+        // Categories classifying the artwork
+        // The medium of the artwork (e.g. "watercolor")
+        categories: {
+            type: "SimpleString",
+            title: (i18n) => i18n.gettext("Categories"),
+            multiple: true,
+            searchField: "filter",
+        },
     },
 };
