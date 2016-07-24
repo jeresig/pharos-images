a5fca206c4d41c5f185076d992d896e990e7623d
John Resig <jeresig@gmail.com>
2016-07-23 21:12:34 -0600

    Specify some default settings and no longer require an options file.

--- tests/options.js
+++ tests/options.js
@@ -51,30 +51,6 @@ const types = {
 };
 
 module.exports = {
-    getShortTitle: () => "Mongaku",
-
-    getTitle() {
-        return this.getShortTitle();
-    },
-
-    getSearchPlaceholder: () => "",
-
-    recordTitle(record) {
-        return record.title;
-    },
-
-    converters: {},
-
-    views: {},
-
-    locales: {},
-
-    filters: [],
-
-    display: [],
-
-    sorts: {},
-
     model: {
         // The title of the artwork.
         title: {
